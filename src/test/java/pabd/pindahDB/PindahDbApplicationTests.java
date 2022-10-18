679aafd7b7dae525a3cb0db193c68541f97d1b8d
akbar <anugrahakbar522@gmmail.com>
2022-10-18 18:33:23 +0700

    TugasPertama

---
+++ src/test/java/pabd/pindahDB/PindahDbApplicationTests.java
@@ -0,0 +1,13 @@
+package pabd.pindahDB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PindahDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
